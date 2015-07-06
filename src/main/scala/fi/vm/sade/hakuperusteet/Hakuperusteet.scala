b4edd76d717901782b6e55066f4a7f61410b90a3
Jani Rahkola <jani.rahkola@reaktor.fi>
2015-07-06 20:14:07 +0300

    Port from ENV

--- src/main/scala/fi/vm/sade/hakuperusteet/Hakuperusteet.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/Hakuperusteet.scala
@@ -1,30 +1,15 @@
 package fi.vm.sade.hakuperusteet
 
-import java.util.Properties
-
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
-import org.slf4j.LoggerFactory
+
 
 object Hakuperusteet {
-
-  val logger = LoggerFactory.getLogger(this.getClass)
-
-  def loadProperties(propertiesFilePath: String): Config = {
-    val prop = new Properties()
-    val propertiesFile = getClass().getClassLoader().getResource(propertiesFilePath)
-    logger.info("Using properties file " + propertiesFile.getPath())
-    val propertiesStream = propertiesFile.openStream()
-    prop.load(propertiesStream)
-    propertiesStream.close()
-    Config(prop.getProperty("port").toInt)
-  }
-
   def main(args: Array[String]) {
-    val config: Config = loadProperties("oph-configuration/hakuperusteet.properties")
-    val server = new Server(config.port)
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+    val server = new Server(port)
     val context = new WebAppContext()
     context setContextPath ("/")
     context.setResourceBase("src/main/webapp")
@@ -37,5 +22,3 @@ object Hakuperusteet {
     server.join
   }
 }
-
-case class Config(port: Int)
