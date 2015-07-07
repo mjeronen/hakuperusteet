52711df1fc164db8074506e88180f6ee343e1269
Jani Rahkola <jani.rahkola@reaktor.fi>
2015-07-07 12:38:14 +0300

    Test form parameter validation

--- project/build.scala
+++ project/build.scala
@@ -25,7 +25,8 @@ object HakuperusteetBuild extends Build {
         "org.eclipse.jetty" % "jetty-webapp" % "9.3.0.v20150612" % "container;compile",
         "org.eclipse.jetty" % "jetty-plus" % "9.3.0.v20150612" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0",
-        "org.json4s" %% "json4s-native" % "3.2.11"
+        "org.json4s" %% "json4s-native" % "3.2.11",
+        "org.scalaz" %% "scalaz-core" % "7.1.3"
       )
     )
   )
--- src/main/scala/fi/vm/sade/hakuperusteet/TestServlet.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/TestServlet.scala
@@ -5,26 +5,58 @@ import java.security.MessageDigest
 import java.util.Base64
 
 import fi.vm.sade.hakuperusteet.Types.Oid
+import org.joda.time.LocalDate
+import org.joda.time.format.ISODateTimeFormat
+import org.json4s.native.JsonMethods._
+import org.json4s.JsonDSL._
 import org.scalatra._
 import org.slf4j.LoggerFactory
+
+import scalaz._
+import scalaz.syntax.applicative._
+import scalaz.syntax.validation._
 
 
 class TestServlet(secrets: Map[Oid, String]) extends ScalatraServlet {
 
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  get("/") {
-    val secret = secrets get params("oid")
-    val hashInput: String = Seq("name", "birth-date", "mail", "oid") map (params(_)) mkString ("") + secret.get
-    val query = Map(
-      "name" -> URLEncoder.encode(params("name"), "UTF-8"),
-      "birth-date" -> URLEncoder.encode(params("birth-date"), "UTF-8"),
-      "mail" -> URLEncoder.encode(params("mail"), "UTF-8"),
-      "hash" -> sha256(hashInput)
-    )
-    halt(status = 303, headers = Map("Location" -> url(params("url"), query, false, false, false)))
-  }
-
   private def sha256(input: String): String =
     Base64.getUrlEncoder().encodeToString(MessageDigest.getInstance("SHA-256").digest(input.getBytes("UTF-8")))
+
+  case class Parameters(name: String, birthDate: LocalDate, mail: String, oid: Oid) {
+    def hash(secret: String): String =
+      sha256(Seq(name, birthDate.toString, mail, oid, secret) mkString)
+  }
+
+  private def validateBirthDate(input: String): ValidationNel[String, LocalDate] =
+    try { ISODateTimeFormat.date().parseLocalDate(input).successNel }
+    catch { case ex: IllegalArgumentException => ex.getMessage.failureNel }
+
+  private def validateOid(input: String): ValidationNel[String, Oid] =
+    if (input.nonEmpty) input.successNel
+    else "Empty oid".failureNel
+
+  private def parseParameters(params: Map[String, String]): ValidationNel[String, Parameters] =
+    (validateBirthDate(params("birth-date")) |@| validateOid(params("oid"))) { (birthDate, oid) =>
+      Parameters(params("name"), birthDate, params("mail"), oid)
+    }
+
+  get("/") {
+    parseParameters(params).bitraverse(
+      errors => {
+        contentType = "application/json"
+        halt(status = 400, body = compact(render("errors" -> errors.list)))
+      },
+      parameters => {
+        val query = Map(
+          "name" -> parameters.name,
+          "birth-date" -> parameters.birthDate.toString,
+          "mail" -> parameters.mail,
+          "oid" -> parameters.oid,
+          "hash" -> parameters.hash(secrets get parameters.oid get)
+        ) mapValues (URLEncoder.encode(_, "UTF-8"))
+        halt(status = 303, headers = Map("Location" -> url(params("url"), query, false, false, false)))
+      })
+  }
 }
