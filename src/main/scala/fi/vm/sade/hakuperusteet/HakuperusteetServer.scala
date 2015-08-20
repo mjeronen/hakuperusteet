4acfaa598ea31715372fc90dfd92194dc1b47d64
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-08-20 12:23:23 +0300

    add support for ssl also, vetuma requires this

--- src/main/scala/fi/vm/sade/hakuperusteet/HakuperusteetServer.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/HakuperusteetServer.scala
@@ -1,8 +1,9 @@
 package fi.vm.sade.hakuperusteet
 
 import com.typesafe.config.ConfigFactory
-import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server._
 import org.eclipse.jetty.servlet.DefaultServlet
+import org.eclipse.jetty.util.ssl.SslContextFactory
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 import org.slf4j.LoggerFactory
@@ -11,18 +12,48 @@ object HakuperusteetServer {
   val logger = LoggerFactory.getLogger(this.getClass)
 
   def main(args: Array[String]) {
-    val port = ConfigFactory.load().getInt("port")
-    val server = new Server(port)
-    val context = new WebAppContext()
-    context setContextPath("/hakuperusteet/")
-    context.setResourceBase(getClass.getClassLoader.getResource("webapp").toExternalForm)
-    context.addEventListener(new ScalatraListener)
-    context.addServlet(classOf[DefaultServlet], "/")
+    val portHttp = ConfigFactory.load().getInt("port.http")
+    val portHttps = ConfigFactory.load().getInt("port.https")
 
-    server.setHandler(context)
+    val server = new Server()
+    server.setHandler(createContext)
+    server.setConnectors(createConnectors(portHttp, portHttps, server))
 
     server.start
     server.join
-    logger.info(s"Hakuperusteet-server started on port $port")
+    logger.info(s"Hakuperusteet-server started on ports $portHttp and $portHttps")
+  }
+
+  private def createConnectors(portHttp: Int, portHttps: Int, server: Server): Array[Connector] = {
+    val httpConnector = new ServerConnector(server, new HttpConnectionFactory(new HttpConfiguration))
+    httpConnector.setPort(portHttp)
+    val httpsConnector = createSSLConnector(portHttps, server)
+    val connectors: Array[Connector] = List(httpConnector, httpsConnector).toArray
+    connectors
+  }
+
+  private def createContext: WebAppContext = {
+    val context = new WebAppContext()
+    context setContextPath ("/hakuperusteet/")
+    context.setResourceBase(getClass.getClassLoader.getResource("webapp").toExternalForm)
+    context.addEventListener(new ScalatraListener)
+    context.addServlet(classOf[DefaultServlet], "/")
+    context
+  }
+
+  private def createSSLConnector(port: Int, server: Server) = {
+    val sslContextFactory = new SslContextFactory
+    sslContextFactory.setKeyStoreType("jks")
+    sslContextFactory.setKeyStorePath(this.getClass.getClassLoader.getResource("keystore").getPath)
+    sslContextFactory.setKeyStorePassword("keystore")
+    sslContextFactory.setKeyManagerPassword("keystore")
+
+    val httpsConfig = new HttpConfiguration
+    httpsConfig.setSecurePort(port)
+    httpsConfig.addCustomizer(new SecureRequestCustomizer)
+
+    val https = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(httpsConfig))
+    https.setPort(port)
+    https
   }
 }
