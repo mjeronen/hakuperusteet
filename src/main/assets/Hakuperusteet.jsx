a99f428bae5cafb0a92e1584380b101533ba54ec
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-08-21 15:32:28 +0300

    add dummy react, todo: npm build, sbt-build does not do the trick afterall

---
+++ src/main/assets/Hakuperusteet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Bacon from 'baconjs'
+import _ from 'lodash'
+
+export default class HakuperusteetApp extends React.Component {
+
+  render() {
+    return <div>Hakuperusteet main page</div>
+  }
+
+}
--- src/main/assets/test.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-import Bacon from 'baconjs'
-import _ from 'lodash'
