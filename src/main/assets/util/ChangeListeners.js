062197326aa098747b536d9fb2b7fefcd64d1902
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-08-27 08:40:28 +0300

    simplify further, we have id in target also

--- src/main/assets/util/ChangeListeners.js
+++ src/main/assets/util/ChangeListeners.js
@@ -7,10 +7,8 @@ export function initChangeListeners(dispatcher, events) {
     }
   }
 
-  function checkedChanges(fieldName) {
-    return (e) => {
-      dispatcher.push(events.updateField, { field: fieldName, value: e.target.checked })
-    }
+  function checkedChanges(e) {
+    dispatcher.push(events.updateField, { field: e.target.id, value: e.target.checked })
   }
 
   return { valueChanges: valueChanges, checkedChanges: checkedChanges }
