8650615628dbccd3611a06a5933f4f8e8943f95c
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-08-27 16:29:21 +0300

    store email also

--- src/main/resources/db/migration/V1__createtables.sql
+++ src/main/resources/db/migration/V1__createtables.sql
@@ -5,6 +5,7 @@ CREATE TABLE "user"
   id bigint NOT NULL,
   version bigint NOT NULL,
   henkilo_oid character varying(255),
+  email character varying(255) NOT NULL,
   firstname character varying(255) NOT NULL,
   lastname character varying(255) NOT NULL,
   syntymaaika date NOT NULL,
@@ -13,7 +14,8 @@ CREATE TABLE "user"
   education_level character varying(255),
   education_country character varying(255),
 
-  CONSTRAINT user_pkey PRIMARY KEY (id)
+  CONSTRAINT user_pkey PRIMARY KEY (id),
+  CONSTRAINT user_email UNIQUE (email)
 )
 WITH (
   OIDS=FALSE
