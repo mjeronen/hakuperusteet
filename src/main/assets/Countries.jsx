69524791305f96acd442405bd542c5afb9a134a7
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-08-28 11:38:41 +0300

    handle errors, todo: fixme

--- src/main/assets/Countries.jsx
+++ src/main/assets/Countries.jsx
@@ -13,12 +13,15 @@ export default class Countries extends React.Component {
     const countries =  _.isUndefined(this.props.countries) ? emptyCountries : this.props.countries
 
     var parseValues = function (country) {
+      if (_.isUndefined(country.metadata)) return { }
       const name = country.metadata.filter(function (meta) { return meta.kieli == lang})[0].nimi
       return { id: country.koodiArvo, name: name }
     }
+    var formatted = countries.map(parseValues).filter(function(c) { return !_.isUndefined(c.id)} )
+
     var sortWith = function(n) { return n.name }
     var toOptions = function (country) { return <option key={country.id}>{country.name}</option> }
-    const result = _.sortBy(countries.map(parseValues), sortWith).map(toOptions)
+    const result = _.sortBy(formatted, sortWith).map(toOptions)
     result.unshift(<option key="">Choose..</option>)
 
     return <select id={field} onChange={controller.valueChanges}>
