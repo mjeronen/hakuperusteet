acb8b5eac7417a7393587436944ce2d4d1146c6b
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-07 13:47:39 +0300

    simplify

--- src/main/scala/fi/vm/sade/hakuperusteet/FormRedirectServlet.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/FormRedirectServlet.scala
@@ -21,7 +21,7 @@ class FormRedirectServlet(config: Config, db: HakuperusteetDatabase, signer: RSA
     val userData = userDataFromSession
     val payments = db.findPayments(userData)
     val shouldPay = userData.educationCountry != "Finland"
-    val hasPaid = payments.exists((p: Payment) => p.status.equals(PaymentStatus.ok))
+    val hasPaid = payments.exists(_.status.equals(PaymentStatus.ok))
     compact(render(Map("url" -> generateUrl(host, userData, shouldPay, hasPaid))))
   }
 
