adb23a3d195fa0aa0cce364465bb56db3bc20e6a
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-08 15:19:46 +0300

    use original dummy info

--- src/main/assets/ProgramInfo.jsx
+++ src/main/assets/ProgramInfo.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default class ProgramInfo extends React.Component {
   render() {
     return <section id="program-info">
-      <h1>Selected program</h1>
-      <h2>Security and Mobile Computing – NordSecMob</h2>
-      <h1>Program info</h1>
+      <h1>Security and Mobile Computing – NordSecMob</h1>
       <p>
         Applying to the programme is done via Aalto University's online application system.
         The application period for studies starting in autumn 2015 will be organized
