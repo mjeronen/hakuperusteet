04ba5637153ece0c36d41e21aedf3a98a4387e8e
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-08 12:42:39 +0300

    add link to header

--- src/main/assets/Header.jsx
+++ src/main/assets/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Header extends React.Component {
   render() {
     return <section id="header">
-      <img src="/hakuperusteet/img/opintopolku_large-en.png" />
+      <a href="https://studyinfo.fi/wp2/en/"><img src="/hakuperusteet/img/opintopolku_large-en.png" /></a>
     </section>
   }
 }
