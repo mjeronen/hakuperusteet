d25b53d2fd6fc92e8051ce954ab76cd75cb3fdc1
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-09-09 12:55:36 +0300

    countries sort and localization done as a server side operation

--- src/main/assets/userdata/Countries.jsx
+++ src/main/assets/userdata/Countries.jsx
@@ -11,17 +11,8 @@ export default class Countries extends React.Component {
     const lang = this.props.lang.toUpperCase()
     const emptyCountries = [{ id: "", name: "Choose.."}]
     const countries = this.props.countries ? JSON.parse(this.props.countries) : emptyCountries
-
-    var parseValues = function (country) {
-      if (_.isUndefined(country.metadata)) return { }
-      const name = country.metadata.filter(function (meta) { return meta.kieli == lang})[0].nimi
-      return { id: country.koodiArvo, name: name }
-    }
-    var formatted = countries.map(parseValues).filter(function(c) { return !_.isUndefined(c.id)} )
-
-    var sortWith = function(n) { return n.name }
     var toOptions = function (country) { return <option value={country.id} key={country.id}>{country.name}</option> }
-    const result = _.sortBy(formatted, sortWith).map(toOptions)
+    const result = countries.map(toOptions)
     result.unshift(<option value="" key="-">Choose..</option>)
 
     return <div className="userDataFormRow">
--- src/main/scala/fi/vm/sade/hakuperusteet/koodisto/Countries.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/koodisto/Countries.scala
@@ -9,7 +9,7 @@ import org.json4s.native.Serialization._
 import org.json4s.{NoTypeHints, DefaultFormats, Formats}
 import org.json4s.native.Serialization.{read, write}
 
-case class Countries(countries: List[Country], eeaCountries: List[String]) {
+case class Countries(countries: List[SimplifiedCountry], eeaCountries: List[String]) {
   def shouldPay(educationCountry: String) = !eeaCountries.contains(educationCountry)
 }
 
@@ -20,6 +20,9 @@ object Countries {
   implicit val formats = Serialization.formats(NoTypeHints)
 
   private def countries(p: Config) = read[List[Country]](urlToString("koodisto.countries.url",p))
+    .map(c => SimplifiedCountry(c.koodiArvo,c.metadata.find(_.kieli.equals("EN")).get.nimi))
+    .sortWith((c0,c1) => c0.name.compareTo(c1.name) < 0)
+
   private def eeaCountries(p: Config) = read[Valtioryhma](urlToString("koodisto.eea.countries.url",p))
 
   private def urlToString(url: String, props: Config) = io.Source.fromInputStream(new URL(
@@ -29,6 +32,7 @@ object Countries {
 
 case class Metadata(nimi: String, kieli: String)
 case class Country(koodiArvo: String, metadata: List[Metadata])
+case class SimplifiedCountry(id: String, name: String)
 
 private case class Element(codeElementValue: String)
 private case class Valtioryhma(withinCodeElements: List[Element])
