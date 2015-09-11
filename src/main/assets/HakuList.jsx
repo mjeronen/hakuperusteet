c4b17e7a09acfb2a7bee6c48f153c679b0481214
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-11 19:40:32 +0300

    use disableSubmitAndShowBusy with hakulist

--- src/main/assets/HakuList.jsx
+++ src/main/assets/HakuList.jsx
@@ -3,11 +3,14 @@ import Bacon from 'baconjs'
 import _ from 'lodash'
 
 import HttpUtil from './util/HttpUtil'
+import {disableSubmitAndShowBusy} from './util/HtmlUtils.js'
 
 export default class HakuList extends React.Component {
   onSubmitRedirect(state) {
     return (e) => {
       e.preventDefault()
+      const form = e.target
+      disableSubmitAndShowBusy(form)
       Bacon.fromPromise(HttpUtil.get(state.properties.formRedirectUrl)).onValue((result) => {
         window.location = result.url
       })
@@ -20,6 +23,7 @@ export default class HakuList extends React.Component {
         <p>Continue to application form with following link.</p>
         <form id="redirectToForm" onSubmit={this.onSubmitRedirect(state)} method="GET">
           <input type="submit" name="redirectToForm" value="Proceed to application form" />
+          <img className="ajax-loader hide" src="/hakuperusteet/img/ajax-loader.gif" />
         </form>
       </div>
   }
