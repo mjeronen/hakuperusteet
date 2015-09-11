596cbb74fd4d342fb9c21cfaf96febde9b02ba2a
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-11 10:47:24 +0300

    create simple birthdatevalidator

--- src/main/assets/util/FieldValidator.js
+++ src/main/assets/util/FieldValidator.js
@@ -3,10 +3,21 @@ import _ from 'lodash'
 export function validateField(state, field, value) {
   if (field == "firstName") return validateNonEmptyTextField(value)
   if (field == "lastName") return validateNonEmptyTextField(value)
+  if (field == "birthDate") return validateBirthDate(value)
   return []
 }
 
 function validateNonEmptyTextField(value) {
   if (_.isEmpty(value)) return ["required"]
   return []
+}
+
+function validateBirthDate(value) {
+  if (_.isEmpty(value)) return ["required"]
+  const eightNumbersPattern = /^([0-9]{8})$/;
+  if (eightNumbersPattern.test(value)) {
+    return []
+  } else {
+    return ["invalid"]
+  }
 }
