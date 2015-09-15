075496c32a7efff573a9ecfac2af995d4b244dde
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-15 10:27:26 +0300

    fix stack trace if we don't have tarjonta yet. todo: ajax-loop

--- src/main/assets/ProgramInfo.jsx
+++ src/main/assets/ProgramInfo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import _ from 'lodash'
 
 export default class ProgramInfo extends React.Component {
   render() {
-    const tarjonta = this.props.state.tarjonta
+    const tarjonta = _.isUndefined(this.props.state.tarjonta) ? {name : "", description: ""} : this.props.state.tarjonta
     const name = tarjonta.name
     const description = tarjonta.description
     return <section id="program-info">
