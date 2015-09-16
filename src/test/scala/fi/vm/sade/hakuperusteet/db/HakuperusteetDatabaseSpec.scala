24ec4ad2f11750bdc06ccfee01b2a5045265702a
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-16 18:34:42 +0300

    add happyFlow payment upsert test

--- src/test/scala/fi/vm/sade/hakuperusteet/db/HakuperusteetDatabaseSpec.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/db/HakuperusteetDatabaseSpec.scala
@@ -1,5 +1,41 @@
 package fi.vm.sade.hakuperusteet.db
 
-class HakuperusteetDatabaseSpec {
+import java.util.Date
 
+import fi.vm.sade.hakuperusteet.Configuration
+import fi.vm.sade.hakuperusteet.domain.{User, PaymentStatus, Payment}
+import org.flywaydb.core.Flyway
+import org.scalatest.{BeforeAndAfterEach, BeforeAndAfterAll, Matchers, FlatSpec}
+
+class HakuperusteetDatabaseSpec extends FlatSpec with Matchers with BeforeAndAfterAll with GlobalExecutionContext {
+  behavior of "HakuperusteetDatabase"
+
+  val config = Configuration.props
+  val db = HakuperusteetDatabase.init(config)
+
+  override def beforeAll() = {
+    clearDatabase()
+  }
+
+  private def clearDatabase() {
+    val url = config.getString("hakuperusteet.db.url")
+    val user = config.getString("hakuperusteet.db.username")
+    val password = config.getString("hakuperusteet.db.password")
+    val flyway = new Flyway
+    flyway.setDataSource(url, user, password)
+    flyway.setSchemas("hakuperusteet")
+    flyway.setValidateOnMigrate(false)
+    flyway.clean
+    flyway.migrate
+  }
+
+  it should "should create new session in" in {
+    val user = new User(None, Some("personOid.1.1.1"), "", "", "", new Date(), None, "", "", "", "", "", "")
+    db.upsertUser(user)
+
+    db.findPayments(user).length shouldEqual 0
+    val p = Payment(None, "personOid.1.1.1", new Date(), "refNo", "orderNo", PaymentStatus.ok)
+    db.upsertPayment(p)
+    db.findPayments(user).length shouldEqual 1
+  }
 }
