9b07a483e35a88aaa25886b655e035607e3d37cc
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-17 10:42:51 +0300

    fix tokenauth, ver1, buggy

--- src/main/scala/fi/vm/sade/hakuperusteet/auth/TokenAuthStrategy.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/auth/TokenAuthStrategy.scala
@@ -16,27 +16,41 @@ class TokenAuthStrategy (protected override val app: ScalatraBase, config: Confi
 
   private def request = app.enrichRequest(app.request)
 
+  val tokenName = "oppijaToken"
+
   val json = parse(request.body)
   val token = (json \ "token").extract[Option[String]]
   val idpentityid = (json \ "idpentityid").extract[Option[String]]
 
   def authenticate()(implicit request: HttpServletRequest, response: HttpServletResponse): Option[Session] = {
     (token, idpentityid) match {
-      case (Some(tokenFromRequest), Some(idpentityidFromSession)) if idpentityidFromSession == "oppijaToken" =>
+      case (Some(tokenFromRequest), Some(idpentityidFromSession)) if idpentityidFromSession == tokenName =>
         db.findSessionByToken(tokenFromRequest) match {
           case s @ Some(session) if session.token == tokenFromRequest => s
+          case Some(session) => validateNewTokenAndUpdateSession(session.copy(token = tokenFromRequest))
           case _ => validateTokenAndCreateSession(tokenFromRequest)
         }
       case _ => None
     }
   }
 
-  def validateTokenAndCreateSession(tokenFromRequest: String): Option[Session] = {
+  def validateNewTokenAndUpdateSession(sessionWithNewToken: Session) = {
+    oppijanTunnistus.validateToken(sessionWithNewToken.token) match {
+      case Some(email) =>
+        logger.info(s"Updating $tokenName session for $email")
+        db.upsertSession(sessionWithNewToken)
+        Some(sessionWithNewToken)
+      case _ => None
+    }
+  }
+
+  def validateTokenAndCreateSession(tokenFromRequest: String) = {
     oppijanTunnistus.validateToken(tokenFromRequest) match {
-      case Some(session) =>
-        logger.info(s"Creating new email session for $session")
-        db.upsertSession(session)
-        Some(session)
+      case Some(email) =>
+        val newSession = Session(None, email, tokenFromRequest, tokenName)
+        logger.info(s"Creating new $tokenName session for $email")
+        db.upsertSession(newSession)
+        Some(newSession)
       case _ => None
     }
   }
