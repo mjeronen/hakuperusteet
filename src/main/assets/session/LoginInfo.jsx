808c620ac33747b8d9471fd956245d303e0af8ea
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-21 16:46:59 +0300

    rename key

--- src/main/assets/session/LoginInfo.jsx
+++ src/main/assets/session/LoginInfo.jsx
@@ -5,7 +5,7 @@ import {translation} from '../translations/translations.js'
 export default class LoginInfo extends React.Component {
   render() {
     return <div className="login-info">
-      <p>{translation("login.info.p")}</p>
+      <p>{translation("login.info.text")}</p>
     </div>
   }
 }
