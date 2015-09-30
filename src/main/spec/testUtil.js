d3ab743be5f62c907b50d9421768bde64a8bcdd9
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-09-30 11:51:50 +0300

    append jquery to iframe, copied from va

--- src/main/spec/testUtil.js
+++ src/main/spec/testUtil.js
@@ -6,7 +6,7 @@ const waitIntervalMs = 10
 
 export function S(selector) {
   try {
-    if (!testFrame()) {
+    if (!testFrame() || !testFrame().jQuery) {
       return $([])
     }
     return $(testFrame().document).find(selector)
@@ -52,7 +52,7 @@ export function waitforMilliseconds(ms) {
 }
 
 export function hakuperusteetLoaded() {
-  return $("#testframe").get(0).contentWindow.SESSION_INITED_FOR_TESTING === true
+  return testFrame().SESSION_INITED_FOR_TESTING === true
 }
 
 export function testFrame() {
@@ -60,8 +60,16 @@ export function testFrame() {
 }
 
 export function openPage(path, predicate) {
+  if (!predicate) {
+    predicate = function() { return testFrame().jQuery }
+  }
   return function () {
-    var newTestFrame = $('<iframe>').attr({src: path, width: 1024, height: 800, id: "testframe"})
+    var newTestFrame = $('<iframe>').attr({src: path, width: 1024, height: 800, id: "testframe"}).load(function() {
+      var jquery = document.createElement("script")
+      jquery.type = "text/javascript"
+      jquery.src = "//code.jquery.com/jquery-1.11.1.min.js"
+      $(this).contents().find("head")[0].appendChild(jquery)
+    })
     $("#testframe").replaceWith(newTestFrame)
     return waitUntil(function () {return predicate() }, testTimeoutPageLoad
     ).then(function () {
