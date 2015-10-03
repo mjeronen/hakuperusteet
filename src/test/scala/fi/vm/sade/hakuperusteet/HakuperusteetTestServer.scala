50019e72dd55b5ea135fd4c565577815b050ec87
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-03 14:24:54 +0300

    start mockserver to port 3001 when running HakuperusteetTestServer

--- src/test/scala/fi/vm/sade/hakuperusteet/HakuperusteetTestServer.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/HakuperusteetTestServer.scala
@@ -1,6 +1,10 @@
 package fi.vm.sade.hakuperusteet
 
+import java.io.File
+
 import org.slf4j.LoggerFactory
+
+import scala.sys.process.{Process, ProcessIO}
 
 object HakuperusteetTestServer {
   val logger = LoggerFactory.getLogger(this.getClass)
@@ -9,8 +13,17 @@ object HakuperusteetTestServer {
    * ./sbt "test:run-main fi.vm.sade.hakuperusteet.HakuperusteetTestServer"
    */
   def main(args: Array[String]): Unit = {
+    startMockServer()
     val s = new HakuperusteetServer
     s.runServer()
     logger.info("Started HakuperusteetTestServer")
+  }
+
+  private def startMockServer() {
+    val pb = Process(Seq("node", "server.js"), new File("./mockserver/"), "PORT" -> "3001", "LDAP_PORT" -> "1390")
+    val pio = new ProcessIO(_ => (),
+      stdout => scala.io.Source.fromInputStream(stdout).getLines.foreach(println),
+      _ => ())
+    pb.run(pio)
   }
 }
