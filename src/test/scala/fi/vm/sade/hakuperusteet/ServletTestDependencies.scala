b32f9c06470d39f24d6deef766e4cfa196baab1f
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-03 12:17:33 +0300

    use empty koodisto stuff in unit tests

--- src/test/scala/fi/vm/sade/hakuperusteet/ServletTestDependencies.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/ServletTestDependencies.scala
@@ -4,16 +4,16 @@ import com.typesafe.config.Config
 import fi.vm.sade.hakuperusteet.db.{GlobalExecutionContext, HakuperusteetDatabase}
 import fi.vm.sade.hakuperusteet.email.EmailSender
 import fi.vm.sade.hakuperusteet.google.GoogleVerifier
-import fi.vm.sade.hakuperusteet.koodisto.Koodisto
+import fi.vm.sade.hakuperusteet.koodisto._
 import fi.vm.sade.hakuperusteet.oppijantunnistus.OppijanTunnistus
 
 trait ServletTestDependencies extends GlobalExecutionContext {
   val config = Configuration.props
   val database = HakuperusteetDatabase.init(config)
   val verifier = new DummyVerifier
-  val countries = Koodisto.initCountries(config)
-  val languages = Koodisto.initLanguages(config)
-  val educations = Koodisto.initBaseEducation(config)
+  val countries = Countries(List.empty[SimplifiedCode], List.empty)
+  val languages = Languages(List.empty[SimplifiedCode])
+  val educations = Educations(List.empty[SimplifiedCode])
   val oppijanTunnistus = new DummyOppijanTunnistus(config)
   val emailSender = EmailSender.init(config)
 }
