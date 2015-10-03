481ead90192d393bc18a2b98e7bc885fb54362ac
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-03 13:06:56 +0300

    add instructions

--- src/test/scala/fi/vm/sade/hakuperusteet/HakuperusteetTestServer.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/HakuperusteetTestServer.scala
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory
 object HakuperusteetTestServer {
   val logger = LoggerFactory.getLogger(this.getClass)
 
+  /*
+   * ./sbt "test:run-main fi.vm.sade.hakuperusteet.HakuperusteetTestServer"
+   */
   def main(args: Array[String]): Unit = {
     val s = new HakuperusteetServer
     s.runServer()
