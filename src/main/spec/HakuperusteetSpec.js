3a385cd6b3a7e08385fd1497d86d316a76693a51
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-04 15:36:07 +0300

    use es6 syntax

--- src/main/spec/HakuperusteetSpec.js
+++ src/main/spec/HakuperusteetSpec.js
@@ -1,94 +1,94 @@
 import {expect, done} from 'chai'
 import {openPage, hakuperusteetLoaded, testFrame, logout, takeScreenshot, S, S2} from './testUtil.js'
 
-describe('Page without session', function() {
+describe('Page without session', () => {
   before(openPage("/hakuperusteet", hakuperusteetLoaded))
 
-  it('should show Google login button', function() {
+  it('should show Google login button', () => {
     return S2(".googleAuthentication.login").then((e) => {
       expect(e.length).to.equal(1)
     }).then(done).catch(done)
   })
-  it('should not show Google session', function() {
+  it('should not show Google session', () => {
     expect(S(".googleAuthentication.session").length).to.equal(0)
   })
 
-  it('should show Email login button', function() {
+  it('should show Email login button', () => {
     return S2(".emailAuthentication.login").then((e) => {
       expect(e.length).to.equal(1)
     }).then(done).catch(done)
   })
 
-  it('should not show Email session', function() {
+  it('should not show Email session', () => {
     expect(S(".emailAuthentication.session").length).to.equal(0)
   })
 
-  it('should not show userDataForm', function() {
+  it('should not show userDataForm', () => {
     expect(S("#userDataForm").length).to.equal(0)
   })
 
-  it('should not show educationForm', function() {
+  it('should not show educationForm', () => {
     expect(S("#educationForm").length).to.equal(0)
   })
 
-  it('should not show vetuma start', function() {
+  it('should not show vetuma start', () => {
     expect(S(".vetumaStart").length).to.equal(0)
   })
 
-  it('should not show hakuList', function() {
+  it('should not show hakuList', () => {
     expect(S(".hakuList").length).to.equal(0)
   })
 })
 
-describe('Page with email session - no userdata', function() {
+describe('Page with email session - no userdata', () => {
   before(openPage("/hakuperusteet/#/token/mochaTestToken", hakuperusteetLoaded))
 
-  it('should show email as loggedIn user', function () {
+  it('should show email as loggedIn user', () => {
     return S2(".loggedInAs").then((e) => {
       expect(e.text()).to.equal("mochatest@example.com")
     }).then(done).catch(done)
   })
 
-  it('should not show email login button', function() {
+  it('should not show email login button', () => {
     expect(S(".emailAuthentication.login").length).to.equal(0)
   })
 
-  it('should not show Google login button', function() {
+  it('should not show Google login button', () => {
     expect(S(".googleAuthentication.login").length).to.equal(0)
   })
 
-  it('should not show Google session', function() {
+  it('should not show Google session', () => {
     expect(S(".googleAuthentication.session").length).to.equal(0)
   })
 
-  it('should show logout button', function () {
+  it('should show logout button', () => {
     return S2("#logout").then((e) => {
       expect(e.length).to.equal(1)
     }).then(done).catch(done)
   })
 
-  it('should show userDataForm', function() {
+  it('should show userDataForm', () => {
     return S2("#userDataForm").then((e) => {
       expect(e.length).to.equal(1)
     }).then(done).catch(done)
   })
 
-  it('should not show educationForm', function() {
+  it('should not show educationForm', () => {
     expect(S("#educationForm").length).to.equal(0)
   })
 
-  it('should not show vetuma start', function() {
+  it('should not show vetuma start', () => {
     expect(S(".vetumaStart").length).to.equal(0)
   })
 
-  it('should not show hakuList', function() {
+  it('should not show hakuList', () => {
     expect(S(".hakuList").length).to.equal(0)
   })
 
-  describe('Insert data', function () {
+  describe('Insert data', () => {
     it('initially submit should be disabled', assertSubmitDisabled)
 
-    it('insert firstname', function () {
+    it('insert firstname', () => {
       S("#firstName").val("John")
     })
 
