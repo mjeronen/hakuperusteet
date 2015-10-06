f54d00a9e1856b805e43fbca52aff2faea209c53
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-06 12:24:39 +0300

    animate error message to make it easier to notice

--- src/main/assets/session/AuthenticationError.jsx
+++ src/main/assets/session/AuthenticationError.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import addons from 'react/addons'
+
+var ReactTransitionGroup = React.addons.CSSTransitionGroup
 
 import {translation} from '../translations/translations.js'
 
 export default class AuthenticationError extends React.Component {
   render() {
-    return <div className="authentication-error">
-      <p>{translation("errors.authentication.invalid")}</p>
-    </div>
+    return <ReactTransitionGroup transitionName="loginerror-result" transitionAppear={true}>
+      <div className="authentication-error">
+        <p>{translation("errors.authentication.invalid")}</p>
+      </div>
+    </ReactTransitionGroup>
   }
 }
