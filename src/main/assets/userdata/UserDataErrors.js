4088f6a9b6cea8a35ec46d66dbff38aeae3e9008
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-06 14:36:12 +0300

    validate all userdataform fields

--- src/main/assets/userdata/UserDataErrors.js
+++ src/main/assets/userdata/UserDataErrors.js
@@ -1,6 +1,11 @@
 import _ from 'lodash'
 
-export function requiredFieldMissing(state, fieldName) {
+export function requiredField(state, fieldName) {
   return !_.isEmpty(state.validationErrors) && !_.isEmpty(state.validationErrors[fieldName])
     && _.contains(state.validationErrors[fieldName], "required")
+}
+
+export function invalidField(state, fieldName) {
+  return !_.isEmpty(state.validationErrors) && !_.isEmpty(state.validationErrors[fieldName])
+    && _.contains(state.validationErrors[fieldName], "invalid")
 }
--- src/main/assets/userdata/UserDataErrors.jsx
+++ src/main/assets/userdata/UserDataErrors.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {requiredFieldMissing} from './UserDataErrors.js'
+import {requiredField, invalidField} from './UserDataErrors.js'
 
 import {translation} from '../../assets-common/translations/translations.js'
 
@@ -8,7 +8,13 @@ export default class UserDataErrors extends React.Component {
   render() {
     const state = this.props.state
     return <div className="userDataFormRow">
-      { requiredFieldMissing(state, "personId") ? <span className="error">{translation("userdataform.errors.requiredPersonId")}</span> : null}
+      { requiredField(state, "firstName") ? <span className="error">{translation("userdataform.errors.requiredFirstName")}</span> : null}
+      { requiredField(state, "lastName") ? <span className="error">{translation("userdataform.errors.requiredLastName")}</span> : null}
+      { invalidField(state, "birthDate") ? <span className="error">{translation("userdataform.errors.invalidBirthDate")}</span> : null}
+      { requiredField(state, "personId") ? <span className="error">{translation("userdataform.errors.requiredPersonId")}</span> : null}
+      { requiredField(state, "gender") ? <span className="error">{translation("userdataform.errors.requiredGender")}</span> : null}
+      { requiredField(state, "nativeLanguage") ? <span className="error">{translation("userdataform.errors.requiredNativeLanguage")}</span> : null}
+      { requiredField(state, "nationality") ? <span className="error">{translation("userdataform.errors.requiredNationality")}</span> : null}
     </div>
   }
 }
