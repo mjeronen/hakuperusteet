24b02602fdf0ecc1b828b38c25e9220a9d3dae93
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-12 09:57:19 +0300

    add ajax-loader to programinfo page

--- src/main/assets/ProgramInfo.jsx
+++ src/main/assets/ProgramInfo.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import _ from 'lodash'
 
+import AjaxLoader from './util/AjaxLoader.jsx'
 import {tarjontaForHakukohdeOid} from "./util/TarjontaUtil.js"
 
 export default class ProgramInfo extends React.Component {
   render() {
-    const tarjonta = tarjontaForHakukohdeOid(this.props.state, this.props.state.hakukohdeOid)
+    const state = this.props.state
+    const tarjonta = tarjontaForHakukohdeOid(state, state.hakukohdeOid)
     const name = tarjonta.name
     const description = tarjonta.description
-    return <section id="program-info">
-      <h1>{name}</h1>
-      <p dangerouslySetInnerHTML={{__html: description}}/>
+    const isLoading = !_.isEmpty(state.hakukohdeOid) && _.isEmpty(name) && _.isEmpty(description)
+
+  return <section id="program-info">
+      { isLoading ?
+        <AjaxLoader hide={false} />
+        :
+        <div>
+          <h1>{name}</h1>
+          <p dangerouslySetInnerHTML={{__html: description}}/>
+        </div>
+      }
     </section>
   }
 }
