dee6ce49883315b713e3db1ce0a6a601b0d4d299
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-13 13:33:58 +0300

    add missing hakuOid to hakukohdequery

--- src/main/scala/fi/vm/sade/hakuperusteet/tarjonta/Tarjonta.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/tarjonta/Tarjonta.scala
@@ -6,14 +6,14 @@ import org.json4s.native.Serialization
 import org.json4s.native.Serialization._
 import fi.vm.sade.hakuperusteet.util.HttpUtil._
 
-case class ApplicationObject(hakukohdeOid: String, name: String, providerName: String, baseEducations: List[String], description: String)
+case class ApplicationObject(hakukohdeOid: String, hakuOid: String, name: String, providerName: String, baseEducations: List[String], description: String)
 case class ApplicationSystem(hakuOid: String, formUrl: String)
 
 case class Tarjonta(tarjontaBaseUrl: String) {
   implicit val formats = Serialization.formats(NoTypeHints)
 
   def getApplicationObject(hakukohdeOid: String) = Option(read[Result](urlToString(tarjontaBaseUrl + "/hakukohde/" + hakukohdeOid))).map(_.result)
-    .map(r => ApplicationObject(r.oid, r.hakukohteenNimet.kieli_en, r.tarjoajaNimet.en, tarjontaUrisToKoodis(r.hakukelpoisuusvaatimusUris), r.lisatiedot.kieli_en)).get
+    .map(r => ApplicationObject(r.oid, r.hakuOid, r.hakukohteenNimet.kieli_en, r.tarjoajaNimet.en, tarjontaUrisToKoodis(r.hakukelpoisuusvaatimusUris), r.lisatiedot.kieli_en)).get
 
   def getApplicationSystem(hakuOid: String) = Option(read[Result2](urlToString(tarjontaBaseUrl + "/haku/" + hakuOid))).map(_.result)
     .map(r => ApplicationSystem(r.oid, r.hakulomakeUri)).get
