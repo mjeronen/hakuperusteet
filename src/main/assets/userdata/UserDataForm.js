0043bfd498118c4327d896adc3e764b788a3be1d
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-14 12:12:38 +0300

    send personId only when not empty

--- src/main/assets/userdata/UserDataForm.js
+++ src/main/assets/userdata/UserDataForm.js
@@ -1,18 +1,21 @@
 import Bacon from 'baconjs'
 import moment from 'moment-timezone'
+import _ from 'lodash'
 
 import HttpUtil from '../util/HttpUtil.js'
 import {enableSubmitAndHideBusy} from '../util/HtmlUtils.js'
 
 export function submitUserDataToServer(state) {
-  const userData = {
+  var userData = {
     firstName: state.firstName,
     lastName: state.lastName,
     birthDate: moment(state.birthDate, "DDMMYYYY").tz('Europe/Helsinki').format("YYYY-MM-DD"),
-    personId: state.personId,
     gender: state.gender,
     nativeLanguage: state.nativeLanguage,
     nationality: state.nationality,
+  }
+  if (!_.isEmpty(state.personId)) {
+    userData.personId = state.personId
   }
   const promise = Bacon.fromPromise(HttpUtil.post(state.properties.userDataUrl, userData))
   promise.onError((error) => {
