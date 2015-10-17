1e7db58e47f5f0e7a62b26ac520804bfbd143ecb
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-10-17 13:04:22 +0300

    rename method

--- src/main/spec/HakuperusteetSpec.js
+++ src/main/spec/HakuperusteetSpec.js
@@ -5,12 +5,12 @@ describe('Page without session', () => {
   before(openPage("/hakuperusteet", hakuperusteetLoaded))
 
   it('should show Google login button', () => {
-    return S2(".googleAuthentication.login").then(assertOneElementFound).then(done).catch(done)
+    return S2(".googleAuthentication.login").then(expectOneElementFound).then(done).catch(done)
   })
   it('should not show Google session', expectNotFound(".googleAuthentication.session"))
 
   it('should show Email login button', () => {
-    return S2(".emailAuthentication.login").then(assertOneElementFound).then(done).catch(done)
+    return S2(".emailAuthentication.login").then(expectOneElementFound).then(done).catch(done)
   })
 
   it('should not show Email session', expectNotFound(".emailAuthentication.session"))
@@ -33,7 +33,7 @@ describe('Page without session - order email token', () => {
   describe('Submit email token order', () => {
     it('click submit should post emailToken', () => { clickField("#session input[name='submit']") })
     it('should show token order success', () => {
-      return S2("#session .success").then(assertOneElementFound).then(done).catch(done)
+      return S2("#session .success").then(expectOneElementFound).then(done).catch(done)
     })
   })
 })
@@ -42,7 +42,7 @@ describe('Page without session - invalid login token', () => {
   before(openPage("/hakuperusteet/#/token/nonExistingToken", hakuperusteetLoaded))
 
   it('should show login error message', () => {
-    return S2(".authentication-error").then(assertOneElementFound).then(done).catch(done)
+    return S2(".authentication-error").then(expectOneElementFound).then(done).catch(done)
   })
 })
 
@@ -51,7 +51,7 @@ describe('Page with email session - userdata', () => {
   before(openPage("/hakuperusteet/ao/1.2.246.562.20.69046715533/#/token/mochaTestToken", hakuperusteetLoaded))
 
   it('should show email as loggedIn user', () => {
-    return S2(".loggedInAs").then(assertOneElementFound).then(done).catch(done)
+    return S2(".loggedInAs").then(expectOneElementFound).then(done).catch(done)
   })
 
   it('should not show email login button', expectNotFound(".emailAuthentication.login"))
@@ -59,10 +59,10 @@ describe('Page with email session - userdata', () => {
   it('should not show Google session', expectNotFound(".googleAuthentication.session"))
 
   it('should show logout button', () => {
-    return S2("#logout").then(assertOneElementFound).then(done).catch(done)
+    return S2("#logout").then(expectOneElementFound).then(done).catch(done)
   })
 
-  it('should show userDataForm', () => { return S2("#userDataForm").then(assertOneElementFound).then(done).catch(done) })
+  it('should show userDataForm', () => { return S2("#userDataForm").then(expectOneElementFound).then(done).catch(done) })
   it('should not show educationForm', expectNotFound("#educationForm"))
   it('should not show vetuma start', expectNotFound(".vetumaStart"))
   it('should not show hakuList', expectNotFound(".hakuList"))
@@ -95,7 +95,7 @@ describe('Page with email session - userdata', () => {
     it('select personId', () => { clickField("#hasPersonId") })
     it('submit should be disabled', assertSubmitDisabled)
     it('show one error after personId is clicked', () => {
-      return S2("#userDataForm .error").then(assertOneElementFound).then(done).catch(done)
+      return S2("#userDataForm .error").then(expectOneElementFound).then(done).catch(done)
     })
 
     it('insert birthDate', () => { return setField("#personId", "-9358") })
@@ -106,14 +106,14 @@ describe('Page with email session - userdata', () => {
   describe('Submit userDataForm', () => {
     it('click submit should post userdata', () => { clickField("input[name='submit']") })
     it('should open educationForm after submit', () => {
-      return S2("#educationForm").then(assertOneElementFound).then(done).catch(done)
+      return S2("#educationForm").then(expectOneElementFound).then(done).catch(done)
     })
   })
 })
 
 describe('Page with email session - educationdata', () => {
   it('should not show userDataForm', expectNotFound("#userDataForm"))
-  it('should show educationForm', () => { return S2("#educationForm").then(assertOneElementFound).then(done).catch(done) })
+  it('should show educationForm', () => { return S2("#educationForm").then(expectOneElementFound).then(done).catch(done) })
   it('should not show vetuma start', expectNotFound(".vetumaStart"))
   it('should not show hakuList', expectNotFound(".hakuList"))
 
@@ -129,21 +129,21 @@ describe('Page with email session - educationdata', () => {
     it('select educationCountry - Finland', () => { return setField("#educationCountry", "246") })
     it('submit should be enabled', assertSubmitEnabled)
     it('should not show missing errors', expectNotFound("#educationForm .error"))
-    it('noPaymentRequired should be visible', () => { return S2(".noPaymentRequired").then(assertOneElementFound).then(done).catch(done) })
+    it('noPaymentRequired should be visible', () => { return S2(".noPaymentRequired").then(expectOneElementFound).then(done).catch(done) })
     it('paymentRequired should be hidden', expectNotFound(".paymentRequired"))
     it('alreadyPaid should be hidden', expectNotFound(".alreadyPaid"))
 
     it('select educationCountry - Solomin Islands', () => { return setField("#educationCountry", "090") })
     it('submit should be enabled', assertSubmitEnabled)
     it('should not show missing errors', expectNotFound("#educationForm .error"))
-    it('paymentRequired should be visible', () => { return S2(".paymentRequired").then(assertOneElementFound).then(done).catch(done)})
+    it('paymentRequired should be visible', () => { return S2(".paymentRequired").then(expectOneElementFound).then(done).catch(done)})
     it('noPaymentRequired should be hidden', expectNotFound(".noPaymentRequired"))
     it('alreadyPaid should be hidden', expectNotFound(".alreadyPaid"))
 
     describe('Submit educationForm', () => {
       it('click submit should post educationdata', () => { clickField("input[name='submit']") })
       it('should show vetuma startpage after submit', () => {
-        return S2(".vetumaStart").then(assertOneElementFound).then(done).catch(done)
+        return S2(".vetumaStart").then(expectOneElementFound).then(done).catch(done)
       })
     })
   })
@@ -152,7 +152,7 @@ describe('Page with email session - educationdata', () => {
 describe('Page with email session - vetuma start page', () => {
   it('should not show userDataForm', expectNotFound("#userDataForm"))
   it('should not show educationForm', expectNotFound("#educationForm"))
-  it('should show vetuma start', () => { return S2(".vetumaStart").then(assertOneElementFound).then(done).catch(done) })
+  it('should show vetuma start', () => { return S2(".vetumaStart").then(expectOneElementFound).then(done).catch(done) })
   it('should not show hakuList', expectNotFound(".hakuList"))
 
   // input name=submit is not allowed when doing redirect, hence different name than in other forms
@@ -161,11 +161,11 @@ describe('Page with email session - vetuma start page', () => {
   describe('Submit vetumaForm', () => {
     it('click submit should go to vetuma and return back with successful payment', () => { clickField("input[name='submitVetuma']") })
     it('should show successful payment as result', () => {
-      return S2(".vetumaResult").then(assertOneElementFound).then(done).catch(done)
+      return S2(".vetumaResult").then(expectOneElementFound).then(done).catch(done)
     })
 
     it('redirectForm should be visible', () => {
-      return S2(".redirectToForm").then(assertOneElementFound).then(done).catch(done)
+      return S2(".redirectToForm").then(expectOneElementFound).then(done).catch(done)
     })
   })
 })
@@ -174,14 +174,14 @@ describe('Page with email session - hakulist page', () => {
   it('should not show userDataForm', expectNotFound("#userDataForm"))
   it('should not show educationForm', expectNotFound("#educationForm"))
   it('should not show vetuma start', expectNotFound(".vetumaStart"))
-  it('should show hakuList', () => { return S2(".hakuList").then(assertOneElementFound).then(done).catch(done) })
+  it('should show hakuList', () => { return S2(".hakuList").then(expectOneElementFound).then(done).catch(done) })
 
   it('initially submit should be enabled', () => { return S2("input[name='redirectToForm']").then(expectToBeEnabled).then(done).catch(done)})
 
   describe('Submit hakulist form', () => {
     it('click submit should redirect to form', () => { clickField("input[name='redirectToForm']") })
     it('should show mock form', () => {
-      return S2(".mockRedirect").then(assertOneElementFound).then(done).catch(done)
+      return S2(".mockRedirect").then(expectOneElementFound).then(done).catch(done)
     })
   })
 })
@@ -190,11 +190,11 @@ describe('Page with email session - add second application object', () => {
   before(openPage("/hakuperusteet/ao/1.2.246.562.20.31077988074#/token/mochaTestToken", hakuperusteetLoaded))
 
   it('should show email as loggedIn user', () => {
-    return S2(".loggedInAs").then(assertOneElementFound).then(done).catch(done)
+    return S2(".loggedInAs").then(expectOneElementFound).then(done).catch(done)
   })
 
   it('should not show userDataForm', expectNotFound("#userDataForm"))
-  it('should show educationForm', () => { return S2("#educationForm").then(assertOneElementFound).then(done).catch(done) })
+  it('should show educationForm', () => { return S2("#educationForm").then(expectOneElementFound).then(done).catch(done) })
   it('should not show vetuma start', expectNotFound(".vetumaStart"))
   it('should not show hakuList', expectNotFound(".hakuList"))
 
@@ -210,7 +210,7 @@ describe('Page with email session - add second application object', () => {
     it('select educationCountry - Finland', () => { return setField("#educationCountry", "246") })
     it('submit should be enabled', assertSubmitEnabled)
     it('should not show missing errors', expectNotFound("#educationForm .error"))
-    it('noPaymentRequired should be visible', () => { return S2(".noPaymentRequired").then(assertOneElementFound).then(done).catch(done) })
+    it('noPaymentRequired should be visible', () => { return S2(".noPaymentRequired").then(expectOneElementFound).then(done).catch(done) })
     it('paymentRequired should be hidden', expectNotFound(".paymentRequired"))
     it('alreadyPaid should be hidden', expectNotFound(".alreadyPaid"))
 
@@ -219,7 +219,7 @@ describe('Page with email session - add second application object', () => {
     it('should not show missing errors', expectNotFound("#educationForm .error"))
     it('noPaymentRequired should be hidden', expectNotFound(".noPaymentRequired"))
     it('paymentRequired should be hidden', expectNotFound(".paymentRequired"))
-    it('alreadyPaid should be displayed', () => { return S2(".alreadyPaid").then(assertOneElementFound).then(done).catch(done)})
+    it('alreadyPaid should be displayed', () => { return S2(".alreadyPaid").then(expectOneElementFound).then(done).catch(done)})
 
     describe('Submit educationForm', () => {
       it('click submit should post educationdata', () => { clickField("input[name='submit']") })
@@ -233,14 +233,14 @@ describe('Page with email session - add second application object', () => {
 describe('Page with email session - no new ao but two existing', () => {
   before(openPage("/hakuperusteet/#/token/mochaTestToken", hakuperusteetLoaded))
 
-  it('should show email as loggedIn user', () => { return S2(".loggedInAs").then(assertOneElementFound).then(done).catch(done) })
+  it('should show email as loggedIn user', () => { return S2(".loggedInAs").then(expectOneElementFound).then(done).catch(done) })
   it('should not show userDataForm', expectNotFound("#userDataForm"))
   it('should not show educationForm', expectNotFound("#educationForm"))
   it('should not show vetuma start', expectNotFound(".vetumaStart"))
-  it('should show hakuList', () => { return S2(".hakuList").then(assertOneElementFound).then(done).catch(done) })
+  it('should show hakuList', () => { return S2(".hakuList").then(expectOneElementFound).then(done).catch(done) })
 })
 
-function assertOneElementFound(e) { expect(e.length).to.equal(1)}
+function expectOneElementFound(e) { expect(e.length).to.equal(1)}
 
 function assertSubmitDisabled() { return S2("input[name='submit']").then(expectToBeDisabled).then(done).catch(done) }
 function assertSubmitEnabled() { return S2("input[name='submit']").then(expectToBeEnabled).then(done).catch(done)}
