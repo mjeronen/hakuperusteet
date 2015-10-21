d670390da04562304f397a8a45bcd2f8c474c6d2
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-10-21 16:41:14 +0300

    baseeducations added to test data

--- src/test/scala/fi/vm/sade/hakuperusteet/domain/ApplicationObjects.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/domain/ApplicationObjects.scala
@@ -2,9 +2,9 @@ package fi.vm.sade.hakuperusteet.domain
 
 object ApplicationObjects {
   private def hakuOid = "1.2.246.562.29.80171652938"
-  private def hakukohdeOid = List("1.2.246.562.20.69046715533", "1.2.246.562.20.31077988074")
+  private def hakukohdeOid = List(("1.2.246.562.20.69046715533","102"), ("1.2.246.562.20.31077988074", "100"))
 
   def generateApplicationObject(u: User): List[ApplicationObject] = {
-    Range(0,2).map(value => ApplicationObject(None, u.personOid.get, hakukohdeOid(value), hakuOid, "102", "008")).toList
+    Range(0,2).map(value => hakukohdeOid(value) match {case(hakukohde,baseEducation) => ApplicationObject(None, u.personOid.get, hakukohde, hakuOid, baseEducation, "008")}).toList
   }
 }
