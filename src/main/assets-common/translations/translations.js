853c2d95943d1f7981bd3693286cf58a89ed1c7e
Mikko Apo <mikko.apo@reaktor.fi>
2015-11-18 20:57:56 +0200

    new translations.json and resolve language from cookie or domain or url

--- src/main/assets-common/translations/translations.js
+++ src/main/assets-common/translations/translations.js
@@ -5,14 +5,40 @@ import * as translations from './translations.json'
 const flatTrans = Flatten(translations)
 
 export function translation(key) {
-  const lang = "en"
-  const fullKey = key + "." + lang
-  const trans = flatTrans[fullKey]
-  if(_.isEmpty(trans)) {
-    console.log("Missing key " + fullKey)
-    return fullKey
-  } else {
-    return trans
-  }
+    const lang = resolveLang()
+    const fullKey = key + "." + lang
+    const trans = flatTrans[fullKey]
+    if (_.isEmpty(trans)) {
+        console.log("Missing key " + fullKey)
+        return fullKey
+    } else {
+        return trans
+    }
+}
 
+export function setLang(val) {
+    document.cookie="i18next="+val+"; path=/";
+}
+
+export function resolveLang() {
+    return getCookie("i18next") || resolveLangFromBrowserUrl() || "en"
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function resolveLangFromBrowserUrl() {
+    const urlBased = {
+        "en": ["studyinfo.fi", "lang=en"],
+        "sv": ["studieinfo.fi", "lang=sv"],
+        "fi": ["opintopolku.fi", "lang=fi"]
+    };
+    return Object.keys(urlBased).filter(function (key) {
+            return urlBased[key].some(function (url) {
+                return location.href.indexOf(url) !== -1
+            })
+        })[0]
 }
