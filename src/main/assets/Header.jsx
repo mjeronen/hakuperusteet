1ad30f3d9f55a9ccbb7a9f91cd142ef8963197c0
Mikko Apo <mikko.apo@reaktor.fi>
2015-11-19 18:31:15 +0200

    Language change link style looks better

--- src/main/assets/Header.jsx
+++ src/main/assets/Header.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 
-import {translation} from '../assets-common/translations/translations.js'
+import {resolveLang} from '../assets-common/translations/translations.js'
 
 export default class Header extends React.Component {
   render() {
-    const controller = this.props.controller
+    const controller = this.props.controller;
+    const lang = resolveLang();
+    const langLinks = [];
+    var langLink1, langLink2;
+    if(lang!=="en") {
+      langLinks.push(<li><a href="#" onClick={controller.changeLang("en")}>In English</a></li>)
+    }
+    if(lang!=="sv") {
+      langLinks.push(<li><a href="#" onClick={controller.changeLang("sv")}>På svenska</a></li>)
+    }
+    if(lang!=="fi") {
+      langLinks.push(<li><a href="#" onClick={controller.changeLang("fi")}>Suomeksi</a></li>)
+    }
+    [langLink1, langLink2] = langLinks;
+
     return <section id="header">
       <div className="headerContent">
         <a href="https://studyinfo.fi/wp2/en/"><img src="/hakuperusteet/img/opintopolku_large-en.png" /></a>
       </div>
-      <a id="changeLangEn" href="#" onClick={controller.changeLang("en")}>In English</a>
-      <a id="changeLangSv" href="#" onClick={controller.changeLang("sv")}>På svenska</a>
-      <a id="changeLangFi" href="#" onClick={controller.changeLang("fi")}>Suomeksi</a>
+      <div className="languageSelector">
+        <ul>
+          {langLink1}
+          {langLink2}
+        </ul>
+      </div>
     </section>
   }
 }
